02a66a2ec35613a0cbbc312dc932f0d0d0722669
TomashTom <smolskijt@gmail.com>
2022-09-26 20:45:02 +0300

    Add project files.

---
+++ ServerSide/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using ServerSide.Models;
+
+namespace ServerSide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+
+            var dblist = dbClient.GetDatabase("myproject").GetCollection<Department>("Department").AsQueryable();
+
+            return new JsonResult(dblist);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            int lastDepartmentId = dbClient.GetDatabase("myproject").GetCollection<Department>("Department").AsQueryable().Count();
+            dep.DepartmentId = lastDepartmentId + 1;
+            dbClient.GetDatabase("myproject").GetCollection<Department>("Department").InsertOne(dep);
+            return new JsonResult("Added Seccessfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", dep.DepartmentId);
+            var update = Builders<Department>.Update.Set("DepartmentName", dep.DepartmentName);
+            
+            dbClient.GetDatabase("myproject").GetCollection<Department>("Department").InsertOne(dep);
+            return new JsonResult("Update Seccessfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", id);
+            dbClient.GetDatabase("myproject").GetCollection<Department>("Department").DeleteOne(filter);
+            return new JsonResult("Delete Seccessfully");
+        }
+    }
+}
---
+++ ServerSide/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using ServerSide.Models;
+using System.Text;
+
+namespace ServerSide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+
+            var dblist = dbClient.GetDatabase("myproject").GetCollection<Employee>("Employee").AsQueryable();
+
+            return new JsonResult(dblist);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Employee emp)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            int lastEmployeeId = dbClient.GetDatabase("myproject").GetCollection<Employee>("Employee").AsQueryable().Count();
+            emp.EmployeeId = lastEmployeeId + 1;
+            dbClient.GetDatabase("myproject").GetCollection<Employee>("Department").InsertOne(emp);
+            return new JsonResult("Added Seccessfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Employee emp)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Employee>.Filter.Eq("EmployeeId", emp.EmployeeId);
+            var update = Builders<Employee>.Update.Set("EmployeeName", emp.EmployeeName)
+                                                                       .Set("Department", emp.Department)
+                                                                       .Set("DateOfJoining", emp.DateOfJoining)
+                                                                       .Set("PhotoFileName", emp.PhotoFileName)
+                                                                      
+                ;
+
+            dbClient.GetDatabase("myproject").GetCollection<Employee>("Employee").InsertOne(emp);
+            return new JsonResult("Update Seccessfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Employee>.Filter.Eq("EmployeeId", id);
+            dbClient.GetDatabase("myproject").GetCollection<Employee>("Employee").DeleteOne(filter);
+            return new JsonResult("Delete Seccessfully");
+        }
+    }
+}
+
